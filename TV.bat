01cd76807ba2f7b04d2d2900cb2326dea5756951
nullprop <git@nullprop.sh>
2018-11-04 03:05:56 +0200

    Initial public release
    Fix cd command

---
+++ TV.bat
@@ -0,0 +1,41 @@
+:: python song request
+cd /d %~dp0\..
+IF NOT EXIST python_sr (
+	git clone https://github.com/nullprop/python_sr
+)
+cd /d ".\python_sr"
+
+:: update song request
+git pull
+call pip install -r ./requirements.txt
+
+:: run song request
+start "python_sr" cmd /c "python ./sr.py"
+
+
+:: demo tools
+cd /d %~dp0\..
+IF NOT EXIST .\demotools (
+	git clone https://github.com/nullprop/demotools
+)
+cd /d ".\demotools"
+
+:: update demo tools
+git pull
+call npm i
+
+
+:: tempustv
+cd /d %~dp0\..
+IF NOT EXIST .\tempustv (
+	git clone https://github.com/nullprop/tempustv
+)
+cd /d ".\tempustv"
+
+:: update tempustv
+git pull
+call npm i
+
+:: run tempustv
+node "app.js"
+pause
